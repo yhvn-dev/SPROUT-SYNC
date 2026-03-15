ae704efa670e9418bf390ea30753ab39e23bb2f1
yhun <jhunguidef@gmail.com>
2026-03-15 22:21:14 +0800

    Remove Service worker on main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,13 @@ import './styles.css'
 import App from './App.jsx'
 import { UserProvider } from './hooks/userContext.jsx'
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(() => console.log('✅ SW registered'))
-      .catch((err) => console.log('❌ SW failed', err));
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js')
+//       .then(() => console.log('✅ SW registered'))
+//       .catch((err) => console.log('❌ SW failed', err));
+//   });
+// }
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
