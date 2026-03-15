85dcb832ba74d6c55b5a3744f8a5d249c6641cb5
yhun <jhunguidef@gmail.com>
2026-03-15 22:07:32 +0800

    Fixed suspense frontend problem

--- client/src/pages/Home/home.jsx
+++ client/src/pages/Home/home.jsx
@@ -360,74 +360,65 @@ function Home() {
 
           </section>
 
-        {/* Install Button */}
-        <InstallButton />
+          {/* Install Button */}
+          <InstallButton />
 
-         
-         {/* Features Section */}
-        <section id="features" className="w-full">
-          <Suspense fallback={<div className="text-center py-8">
-            <Features_Skeleton/>
-          </div>}>
-            <FeatureSection />
-          </Suspense>
-        </section>
-
-      
-        {/* Dashboard Section */}
-        <section className='w-full flex justify-center' id="dashboard_mockup">
-            <Suspense fallback={
-            <Dashboard_Mockup_Skeleton/>
-          }>       
-          </Suspense> 
-       
-          <Dashboard_Mockup />   
-        </section>
-
-
-        {/* Farm Info Section */}
-        <section id="farm" className="w-full">
-          <Suspense fallback={
-              <div className="text-center py-8">
-              <Farm_Info_Skeleton/>
-              </div>}>
-          </Suspense>
-          <Farm_Info_Section />
-        </section>
-
-
-
-        {/* Benefits Section */}
-        <section className="w-full">
-          <Suspense fallback={<div className="text-center py-8 ">
-             <BenefitSection_Skeleton/>
-          </div>}>
-          </Suspense>
-
-           <BenefitSection />
-        </section>
-
-
-        {/* Logo Section */}
-        <section className="w-full">
-          
-          <Suspense fallback={
-            <div className="text-center py-8">     
-                 <Logo_Page_Skeleton/>
-            </div>}>
-            <Logo_Page />
         
-          </Suspense>
-        </section>
+          {/* Features Section */}
+          <section id="features" className="w-full">
+            <Suspense fallback={<div className="text-center py-8"><Features_Skeleton/></div>}>
+              <FeatureSection />
+            </Suspense>
+          </section>
+
+          {/* Dashboard Section */}
+          <section className='w-full flex justify-center' id="dashboard_mockup">
+            <Suspense fallback={<Dashboard_Mockup_Skeleton/>}>
+              <Dashboard_Mockup />
+            </Suspense>
+          </section>
+
+          {/* Farm Info Section */}
+          <section id="farm" className="w-full">
+            <Suspense fallback={<div className="text-center py-8"><Farm_Info_Skeleton/></div>}>
+              <Farm_Info_Section />
+            </Suspense>
+          </section>
+
+          {/* Benefits Section */}
+          <section className="w-full">
+            <Suspense fallback={<div className="text-center py-8"><BenefitSection_Skeleton/></div>}>
+              <BenefitSection />
+            </Suspense>
+          </section>
+
+          {/* Logo Section */}
+          <section className="w-full">
+            <Suspense fallback={<div className="text-center py-8"><Logo_Page_Skeleton/></div>}>
+              <Logo_Page />
+            </Suspense>
+          </section>
+  
+          {/* Logo Section */}
+          <section className="w-full">
+            
+            <Suspense fallback={
+              <div className="text-center py-8">     
+                  <Logo_Page_Skeleton/>
+              </div>}>
+              <Logo_Page />
+          
+            </Suspense>
+          </section>
 
 
-        <section className='w-full p-8'>
-          <Suspense fallback={<div className="text-center py-8">
-            <Footer_Skeleton/>
-          </div>}>
-              <Footer />  
-           </Suspense>        
-        </section>
+          <section className='w-full p-8'>
+            <Suspense fallback={<div className="text-center py-8">
+              <Footer_Skeleton/>
+            </div>}>
+                <Footer />  
+            </Suspense>        
+          </section>
 
       
         </>
